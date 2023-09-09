f5b8612fc0baa0c2640e7acf8d78451660b3a7ed
Dongli Liu <dongliliu0@gmail.com>
2023-09-09 14:00:38 -0700

    finished d_places, mean, median, mode, v_p, v_s

--- StatisticsSolver.py
+++ StatisticsSolver.py
@@ -6,18 +6,64 @@ Created on Sat Sep  9 10:11:15 2023
 """
 
 class StatisticsSolver:
+    def max_decimal_places(self, nums):
+        max_places = 0
+        for num in nums:
+            if '.' in str(num):
+                decimal_places = len(str(num).split('.')[1])
+                max_places = max(max_places, decimal_places)
+        return max_places
+    
     def mean(self, data):
-        return sum(data) / len(data)
+        mean = sum(data) / len(data)
+        decimal_places = self.max_decimal_places(data)
+        mean = round(mean, decimal_places + 1)
+        return mean
 
     def median(self, data):
         data.sort()
-        if len(data)
+        n = len(data)
+        decimal_places = self.max_decimal_places(data)
+        if n%2 == 0:
+            median = (data[n//2-1] + data[n//2])/2
+            median = round(median, decimal_places + 1)
+        else:
+            median = data[(n-1)//2]
+        return median
 
     def mode(self, data):
-        pass
+        frequency_dict = {}
+        for num in data:
+            if num in frequency_dict:
+                frequency_dict[num] += 1
+            else:
+                frequency_dict[num] = 1
+        
+        max_frequency = max(frequency_dict.values())
+        mode_values = [num for num, frequency in frequency_dict.items() if frequency == max_frequency]
+        
+        if max_frequency == 1 or len(mode_values) == len(data):
+            return "No mode"
+        elif len(mode_values) == 1:
+            return mode_values[0]
+        else:
+            return mode_values
 
-    def variance(self, data):
-        pass
+    def variance_p(self, data):
+        mean = self.mean(data)
+        var_list = [(x-mean)*(x-mean) for x in data]
+        variance = sum(var_list)/len(data)
+        decimal_places = self.max_decimal_places(data)
+        variance = round(variance, decimal_places+1)
+        return variance
+
+    def variance_s(self, data):
+        mean = self.mean(data)
+        var_list = [(x-mean)*(x-mean) for x in data]
+        variance = sum(var_list)/(len(data)-1)
+        decimal_places = self.max_decimal_places(data)
+        variance = round(variance, decimal_places+1)
+        return variance
 
     def standard_deviation(self, data):
         pass
@@ -26,16 +72,18 @@ class StatisticsSolver:
 # Example Usage:
 solver = StatisticsSolver()
 
-data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+data = [1, 2, 3, 4, 8, 8, 7, 6, 7, 9, 10]
 
 mean_value = solver.mean(data)
 median_value = solver.median(data)
 mode_value = solver.mode(data)
-variance_value = solver.variance(data)
+variance_value_population = solver.variance_p(data)
+variance_value_sample = solver.variance_s(data)
 std_deviation_value = solver.standard_deviation(data)
 
 print(f"Mean: {mean_value}")
 print(f"Median: {median_value}")
 print(f"Mode: {mode_value}")
-print(f"Variance: {variance_value}")
+print(f"Variance: {variance_value_population}")
+print(f"Variance: {variance_value_sample}")
 print(f"Standard Deviation: {std_deviation_value}")
