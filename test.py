890fec1bfc3f213aca7cfae9cbc6565d60eb9fac
Dongli Liu <dongliliu0@gmail.com>
2023-09-26 20:46:34 -0700

    split DescriptiveBivariate, added n_c_x in utils

--- test.py
+++ test.py
@@ -1,67 +1,35 @@
-import base
-from descriptive import *
+from Descriptive import DescriptiveStat
 from utils import print_dict
+from DescriptiveBivariate import DescriptiveBivariate
 
 # Example Usage:
 
-data = [62,42,19,26,51,15,30,15]
+data = [115, 118, 113, 128, 123, 110, 112]
 solver = DescriptiveStat(data)
 
-'''
-weights = [2, 4, 5, 1, 9, 3, 4, 6, 8, 3, 2, 4, 5, 1, 9, 3, 4, 6, 
-           8, 3, 2, 4, 5, 1, 9, 3, 4, 6, 8, 3]
-x = 42
-p = 90
-a = 10
-b = 90
-
-mean_value = solver.mean() 
-median_value = solver.median()
-mode_value = solver.mode()
-variance_value_population = solver.variance()
-sd_value_population = solver.standard_deviation()
-data_range = solver.range()
-midrange = solver.midrange()
-# weighted_mean = solver.weighted_mean(data, weights)
-z_population = solver.z_score(x)
-get_percentile = solver.percentile_get_p(x)
-get_percentile
-get_value_from_p = solver.percentile_get_x(p)
-quartiles = solver.quartile_and_more()
-a2b_percentile_range = solver.a2b_percentile_range(a, b)
-outlier_check = solver.outlier_check(x)
-
-
-print(f"Mean: {mean_value}")
-print(f"Median: {median_value}")
-print(f"Mode: {mode_value}")
-print(f"Variance p: {variance_value_population}")
-print(f"Standard deviation: {sd_value_population}")
-print(f"Range: {data_range}")
-print(f"Midrange: {midrange}")
-# print(f"Weighted mean: {weighted_mean}")
-print(f"z score population of {x}: {z_population}")
-print(f"Percentile of {x}: {get_percentile}")
-print(f"value when p = {p}: {get_value_from_p}") 
-print("Quartiles:")
-print_dict(quartiles)
-print(f"P{a} to P{b} percentile range: {a2b_percentile_range}")
-print(f'{x} is {outlier_check}')
-'''
+p55 = solver.percentile_get_x(75)
+mean = solver.mean()
+print(f'mean: {mean}')
+median = solver.median()
+more = solver.quartile_and_more()
+print(f'percentile 55 is: {p55}')
+print(f'median of data is: {median}')
+print_dict(more)
 
 # x = [72,69,58,47,84,62,57,45]
 # y = [62,42,19,26,51,15,30,15]
-x = [1,2,3,4]
-y = [2,4,4,6]
+x = [9, 4, 5, 8, 6, 7]
+y = [50, 102, 83, 68, 85, 92]
 
 bi_v = DescriptiveBivariate(x,y)
 regression_line = bi_v.linear_regression()
 correlation_coeff = bi_v.correlation_coefficient()
-coefficient_of_determination = bi_v.coefficient_of_determination()
+coefficient_of_determination = bi_v.coefficient_of_determination(False)
 
 print('regression line formula: ')
 print_dict(regression_line)
 print(f'correlation coeffient: {correlation_coeff}')
 print(f'coefficient_of_determination: {coefficient_of_determination}')
+
 
  
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import math
+
 def max_decimal_places(nums):
     max_places = 0
     for num in nums:
@@ -9,3 +11,10 @@ def max_decimal_places(nums):
 def print_dict(d):
     for key, value in d.items():
         print(f'  {key} - {value}')
+        
+
+def n_c_x(n, x):
+    if x < 0 or x > n:
+        return 0
+    else:
+        return math.comb(n, x)
