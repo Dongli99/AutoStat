ccdaae89dec433053338db1567cd1a4cced1dda5
Dongli Liu <dongliliu0@gmail.com>
2023-09-28 23:26:07 -0700

    deleted z0 due to the complexity

--- DescriptiveNormalDistribution.py
+++ DescriptiveNormalDistribution.py
@@ -35,42 +35,8 @@ class DescriptiveNormalDistribution:
     
     def z_score_lookup_between(self, bottom, top):
         return self.z_score_lookup_lower(top) - self.z_score_lookup_lower(bottom)
-            
-    def lower_than_z0(self, value):
-        diff = 1000000
-        tie = False
-        # loop to get the index of value
-        for i in range(len(self.rows)):
-            for j in range(len(self.cols)):
-                if value - self.z_df.iloc[i, j] > diff:
-                    if j == 0:
-                        row_i = i-1
-                        col_i = 9
-                    else:
-                        row_i = i
-                        col_i = j-1
-                    break
-                elif value - self.z_df.iloc[i, j] == diff:
-                    tie = True
-                    if j == 0:
-                        row_i = [i-1, i]
-                        col_i = [9, 0]
-                    else:
-                        row_i = [i, i]
-                        col_i = [j-1, j]
-                    break
-                else:
-                    diff = value - self.z_df.iloc[i, j]
-        if tie:
-            row_v1, col_v1 = self.rows[row_i[0]], self.cols[col_i[0]]
-            row_v2, col_v2 = self.rows[row_i[1]], self.cols[col_i[1]]
-            v1 = row_v1 + col_v1
-            v2 = row_v2 + col_v2
-            v = (v1+v2)/200 
-        else:
-            v = (self.rows[row_i] + self.cols[col_i])/100
-        return v
-            
+ 
+    
             
 
                 
--- test.py
+++ test.py
@@ -47,6 +47,9 @@ nd = DescriptiveNormalDistribution()
 ztest = nd.z_score_lookup_lower(-0.01)
 print(ztest)
 
+find_z0 = nd.lower_than_z0(0.0002)
+print(f'z lower than z0, z0 is {find_z0}')
+
 
 
 
